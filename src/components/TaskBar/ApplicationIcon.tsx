88254c36c675d06fa58dd151b3c91dd62fbb160a
Adam Hodson <26072111+awphi@users.noreply.github.com>
2025-09-07 19:57:47 +0100

    context menu work

--- src/components/TaskBar/ApplicationIcon.tsx
+++ src/components/TaskBar/ApplicationIcon.tsx
@@ -1,10 +1,14 @@
 import applicationsRegistry from "@/applications";
 import TaskBarIcon from "./Icon";
-import { type MouseEvent, useCallback, useMemo } from "react";
+import { type MouseEvent, useCallback, useMemo, useState } from "react";
 import useAppDispatch from "@/hooks/useAppDispatch";
 import { openApplication, setApplicationProps } from "@/store/applications";
 import { useFocus } from "@/hooks/useFocus";
 import { cn } from "@/utils";
+
+import { ContextMenuTrigger } from "@radix-ui/react-context-menu";
+import { Popover, PopoverAnchor, PopoverContent } from "../Popover";
+import { PopoverPortal, PopoverTrigger } from "@radix-ui/react-popover";
 
 export interface TaskBarApplicationIconProps {
   definitionId: string;
@@ -19,6 +23,7 @@ export default function TaskBarApplicationIcon({
   const Icon = def.icon;
   const dispatch = useAppDispatch();
   const { isFocused, focus } = useFocus();
+  const [contextMenuOpen, setContextMenuOpen] = useState(false);
 
   const isSomeInstanceFocused = useMemo(
     () => applicationIds.some(isFocused),
@@ -41,7 +46,7 @@ export default function TaskBarApplicationIcon({
           );
         }
       } else {
-        // TODO open context menu listing all active windows - clicking one will focus it
+        setContextMenuOpen(true);
       }
 
       // prevent click being captured by parent which will focus(null)
@@ -50,38 +55,51 @@ export default function TaskBarApplicationIcon({
     [definitionId, applicationIds, isSomeInstanceFocused]
   );
 
-  // TODO right click context menu to close all or open a new instance of this application
-
   return (
-    <TaskBarIcon
-      onClick={handleClick}
-      className={cn(
-        {
-          "bg-neutral-200/5": isSomeInstanceFocused,
-        },
-        "relative"
-      )}
-    >
-      {isSomeInstanceFocused && applicationIds.length > 1 ? (
-        <div className="absolute  w-[91%] h-full bg-neutral-200/5 rounded-sm left-0 border-r border-neutral-200/5"></div>
-      ) : null}
-      <div
-        title={def.name}
-        className="h-full flex items-center justify-center relative"
-        style={{ aspectRatio: "1.1 / 1" }}
-      >
-        <Icon />
-        <div
+    <Popover open={contextMenuOpen} onOpenChange={setContextMenuOpen}>
+      <PopoverAnchor>
+        <TaskBarIcon
+          onContextMenu={() => setContextMenuOpen(true)}
+          onClick={handleClick}
           className={cn(
-            "absolute bottom-0.5 w-2 h-1 rounded-sm transition-all",
-            isSomeInstanceFocused ? "bg-primary-400" : "bg-neutral-400",
             {
-              "opacity-0": applicationIds.length === 0,
-              "w-4": isSomeInstanceFocused,
-            }
+              "bg-neutral-200/5": isSomeInstanceFocused,
+            },
+            "relative"
           )}
-        ></div>
-      </div>
-    </TaskBarIcon>
+        >
+          {isSomeInstanceFocused && applicationIds.length > 1 ? (
+            <div className="absolute w-[91%] h-full bg-neutral-200/5 rounded-sm left-0 border-r border-neutral-200/5"></div>
+          ) : null}
+          <div
+            title={def.name}
+            className="h-full flex items-center justify-center relative"
+            style={{ aspectRatio: "1.1 / 1" }}
+          >
+            <Icon />
+            <div
+              className={cn(
+                "absolute bottom-0.5 w-2 h-1 rounded-sm transition-all",
+                isSomeInstanceFocused ? "bg-primary-400" : "bg-neutral-400",
+                {
+                  "opacity-0": applicationIds.length === 0,
+                  "w-4": isSomeInstanceFocused,
+                }
+              )}
+            ></div>
+          </div>
+        </TaskBarIcon>
+      </PopoverAnchor>
+      <PopoverPortal>
+        <PopoverContent
+          side="top"
+          sideOffset={10}
+          className="mx-2 bg-neutral-800/70 backdrop-blur-lg"
+        >
+          TODO - animate motion div that slides up and list all open apps +
+          supports opening a new one
+        </PopoverContent>
+      </PopoverPortal>
+    </Popover>
   );
 }
--- src/components/TaskBar/constants.ts
+++ src/components/TaskBar/constants.ts
@@ -1,1 +1,2 @@
 export const TASK_BAR_HEIGHT = 54;
+export const TASK_BAR_Z_INDEX = 1_000_000;

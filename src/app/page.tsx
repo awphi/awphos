1e6b762288d2ecdfbfc031cd39e0e4758f127574
Adam Hodson <26072111+awphi@users.noreply.github.com>
2025-09-10 18:14:34 +0100

    replace heavyweight image with linear gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,18 @@ import { Provider } from "react-redux";
 import Image from "next/image";
 import { useFocus } from "@/hooks/useFocus";
 
-// TODO more wallpapers once we have user preferences system
-import wallpaperMountain1 from "../../public/wallpapers/mountain-1.jpg";
-
 function HomeContent() {
   const { focus } = useFocus();
 
   return (
     <>
-      <Image
-        className=" -z-50 pointer-events-none"
-        alt="Wallpaper image"
-        fill
-        sizes="100vw"
+      <div
+        className="-z-50 pointer-events-none fixed w-full h-full"
         style={{
-          objectFit: "cover",
+          background:
+            "radial-gradient(circle,rgba(69, 68, 186, 1) 0%, #2a2970 40%, #1c1b4b 80%, rgba(7, 7, 19, 1) 112%)",
         }}
-        src={wallpaperMountain1}
-      ></Image>
+      ></div>
       <div
         className="w-full h-full flex flex-col"
         onClick={() => focus(null)}
